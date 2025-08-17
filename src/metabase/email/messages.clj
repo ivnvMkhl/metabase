055a885a085d31bc4eadb8aa55e7193c294d1507
Михаил Иванов <mihailivanov@MacBook-Pro-Mihail.local>
2025-08-17 12:09:46 +0300

    Правит имя приложения в emails

--- src/metabase/email/messages.clj
+++ src/metabase/email/messages.clj
@@ -53,7 +53,7 @@
   via tru if a name isn't configured."
   []
   (or (public-settings/application-name)
-      (trs "Metabase")))
+      (trs "SPN Report")))
 
 ;; Dev only -- disable template caching
 (when config/is-dev?
